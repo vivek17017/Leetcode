55554168d3c725a88058225ddb14cb340620b3dc
Vivek Kumar <vivekgupta70509@gmail.com>
2025-10-27 13:24:36 +0530

    Time: 3 ms (51.13%) | Memory: 9.9 MB (25.70%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,24 +3,13 @@ public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length())
             return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s!=t)
-         return false;
-       
-
-
-        // for(int i=0;i<s.length();i++)
-        // {
-        //     int count=0;
-        //     for(int j=0;j<t.length();j++)
-        //     {
-        //         if(s[i]==t[j])
-        //             count++;
-        //     }
-        //     if(count==0)
-        //         return false;
-        // }
-        return true;
+        unordered_map<char,int>s1;
+        unordered_map<char,int>t1;
+        for(int i=0;i<s.length();i++)
+        {
+            s1[s[i]]++;
+            t1[t[i]]++;
+        }
+        return s1==t1;
     }
 };
