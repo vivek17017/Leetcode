139999097b73c381cc5e402f1445d1f1f32cc990
Vivek Kumar <vivekgupta70509@gmail.com>
2026-01-26 13:27:47 +0530

    Time: 4 ms (35.48%) | Memory: 9.6 MB (73.88%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length())
-            return false;
-        unordered_map<char,int>s1;
-        unordered_map<char,int>t1;
-        for(int i=0;i<s.length();i++)
-        {
-            s1[s[i]]++;
-            t1[t[i]]++;
-        }
-        return s1==t1;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
