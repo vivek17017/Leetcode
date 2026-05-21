717a391b096ec3e8187485040a10af2b7cf4be21
Vivek Kumar <vivekgupta70509@gmail.com>
2026-05-21 20:23:01 +0530

    Time: 614 ms (7.03%) | Memory: 16.7 MB (89.55%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        while (tokens.size() > 1) {
+            for (int i = 0; i < tokens.size(); i++) {
+                if (tokens[i] == "+"
+                    || tokens[i] == "-"
+                    || tokens[i] == "*"
+                    || tokens[i] == "/")
+                {
+                    int a = stoi(tokens[i - 2]);
+                    int b = stoi(tokens[i - 1]);
+                    int result = 0;
+                    if (tokens[i] == "+") result = a + b;
+                    else if (tokens[i] == "-") result = a - b;
+                    else if (tokens[i] == "*") result = a * b;
+                    else if (tokens[i] == "/") result = a / b;
+
+                    tokens.erase(tokens.begin() + i - 2, tokens.begin() + i + 1);
+                    tokens.insert(tokens.begin() + i - 2, to_string(result));
+                    break;
+                }
+            }
+        }
+        return stoi(tokens[0]);
+    }
+};
